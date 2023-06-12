e3ef55d0b7a79d1d89cff22f40af6eb1bd208307
nouh <www.mndb1@gmail.com>
2023-06-12 16:53:11 +0300

    bugs

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,45 +1,32 @@
 import { Draggable, Droppable } from "react-beautiful-dnd";
-type Props ={
-    id: TypedColumn,
-    todos :Todo[],
-    index :number
-}
-function Column({id,todos,index}:Props) {
+type Props = {
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+function Column({ id, todos, index }: Props) {
   return (
-    <Draggable  draggableId="id" index={index} >
+    <Draggable draggableId="id" index={index}>
       {(provided) => (
-
-      
         <div
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <Droppable droppableId={index.toString()}  type="card"  >
-            {(provided,snapshot)=>(
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
               <div
-              {...provided.droppableProps}
-
-              ref={provided.innerRef}
-              className={`p-2 rounded-2xl shadow-sm ${
-                snapshot.isDraggingOver ? "bg-green-200": "bg-white/50  "
-              }`}
-              ><h2>
-                {id}
-              </h2>
-
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50  "
+                }`}
+              >
+                <h2>{id}</h2>
               </div>
             )}
-
           </Droppable>
-          
-
-
-
-
-
-
-        </div>;
+        </div>
       )}
     </Draggable>
   );
